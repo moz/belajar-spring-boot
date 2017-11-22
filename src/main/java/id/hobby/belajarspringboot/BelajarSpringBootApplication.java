ee03e040f94107a6ee94dca138751c49623def38
Rahman Pujianto <rahmanpujianto@yahoo.com>
2017-11-22 12:56:46 +0700

    Initial commit

---
+++ src/main/java/id/hobby/belajarspringboot/BelajarSpringBootApplication.java
@@ -0,0 +1,12 @@
+package id.hobby.belajarspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringBootApplication.class, args);
+	}
+}
