087242743e9c475289b6159a04e97211863118d8
Rahman Pujianto <rahmanpujianto@gmail.com>
2017-12-12 17:33:07 +0700

    menghasilkan output json

---
+++ src/main/java/id/hobby/belajarspringboot/Hello.java
@@ -0,0 +1,11 @@
+package id.hobby.belajarspringboot;
+
+public class Hello {
+    public String name;
+    public String msg;
+
+    public Hello(String name, String msg) {
+        this.name = name;
+        this.msg = msg;
+    }
+}
--- src/main/java/id/hobby/belajarspringboot/HelloController.java
+++ src/main/java/id/hobby/belajarspringboot/HelloController.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by rahman on 11/22/17.
  */
 @Component
 @Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
 public class HelloController {
     @GET
-    public String hello() {
-        return "Hello world!!!";
+    public Hello hello() {
+        return new Hello("Rafi", "Hello world");
     }
 }
